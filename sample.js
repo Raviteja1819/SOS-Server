7d64d5289fb314b5dad76634b9931698bc57d54a
aryateja1996 <sreekarkondaveeti76@gmail.com>
2024-03-28 13:11:06 +0530

    changes

--- sample.js
+++ sample.js
@@ -12,9 +12,9 @@ const app = express();
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
   // Fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+  // for (let i = 0; i < numCPUs; i++) {
+  //   cluster.fork();
+  // }
   cluster.on('exit', (worker, code, signal) => {
     console.log(`Worker ${worker.process.pid} died`);
     console.log('Forking a new worker...');
